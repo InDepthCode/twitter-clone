ebee83105db5cd6feb783321bc0f943036d1e59e
InDepthCode <amrut.work@gmail.com>
2025-05-13 02:02:54 +0530

    added backend

--- twitter-frontend/src/Components/HomeSection/HomeSection.jsx
+++ twitter-frontend/src/Components/HomeSection/HomeSection.jsx
@@ -4,7 +4,7 @@ const HomeSection = () => {
   return (
     <div className='space-y-5'>
       <section>
-        <h1 className='py-5 text-xl font-bold opacity-90'>Home</h1>
+        <h1 className='py-5 text-xl font-bold opacity-90'>Home page under progress... </h1>
       </section>
     </div>
   )
